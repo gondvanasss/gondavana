615e59eaa1f195d31af57c9d9ed924dd93b50b52
gondvanasss <128378745+gondvanasss@users.noreply.github.com>
2023-03-20 15:50:39 +0400

    Add files via upload

---
+++ crasher.py
@@ -0,0 +1,69 @@
+from turtle import *
+import random, os
+
+os.system('mode con: cols=25 lines=8')
+
+races = {
+    'White' : '#ffdbaf',
+    'Irish' : '#fff2e2',
+    'Black' : '#756046',
+    'African' :'#443421',
+    'Asian' : '#ffe8b2',
+    'Hispanic' : '#edb86f',
+    'Indian' : '#997138',
+    }
+
+racename, racevalue = random.choice(list(races.items()))
+bsize = random.randint(25,80)
+girth = random.randint(25,bsize)
+length = random.randint(150,450)
+xoffset = 0 - (length/2)
+
+print('Race:', racename)
+print('Ball size:',bsize)
+print('Girth:',girth)
+print('Length:',length)
+
+hideturtle()
+setup(length + bsize + 225, bsize*4 + 205)
+title('Dick Window')
+speed(15)
+
+bgcolor('black')
+color('black')
+setx(xoffset)
+
+print('Drawing...')
+color(racevalue) 
+
+
+begin_fill()
+circle(bsize)
+circle(0 - bsize)
+end_fill()
+
+begin_fill()
+sety(0 - girth)
+forward(length)
+end_fill()
+
+
+color('pink')
+begin_fill()
+circle(girth,180)
+end_fill()
+
+
+color(racevalue)
+begin_fill()
+forward(length)
+sety(0)
+
+
+setpos(length + xoffset, 0 - girth)
+setpos(length + xoffset, girth)
+setpos(xoffset,0)
+end_fill()
+
+print('Complete.')
+done()
